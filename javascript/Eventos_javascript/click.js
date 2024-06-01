db3ae0670cbe436adf28add247350aa6a4b8a09e
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-06-01 10:49:36 -0300

    onclik

---
+++ javascript/Eventos_javascript/click.js
@@ -0,0 +1,14 @@
+function umClique(){
+document.getElementById("imagem").src="fachada.jpeg";
+document.getElementById("log").innerHTML+="troca imagem do logo tipo <br> "
+
+
+
+}
+function doisCliques(){
+document.getElementById("imagem").src="ICON INSTA.png";
+document.getElementById("log").innerHTML+=" troca para o logo do ICON <br> "
+
+
+
+}
