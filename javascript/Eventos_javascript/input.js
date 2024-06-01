1bc9850f2a87b2d78109c2e8acd2108ba6557a56
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-06-01 11:31:34 -0300

    input

---
+++ javascript/Eventos_javascript/input.js
@@ -0,0 +1,28 @@
+function submeti(){
+document.getElementById("log").innerHTML+="sumbimeti<br>";
+   return false;
+}
+function resetei(){
+document.getElementById("log").innerHTML+="resetei<br>"
+
+}
+function SairdoCampo(){
+document.getElementById("log").innerHTML+="sai do campo<br>"
+}
+function mudei(){
+document.getElementById("log").innerHTML+="mudei de campo<br>"
+
+}
+function EntreinoCampo(){
+document.getElementById("log").innerHTML+="entrei no campo<br>"
+
+}
+function selecionei(){
+document.getElementById("log").innerHTML+="selecionei<br>"
+}
+function teclabaixo(){
+  document.getElementById("log").innerHTML+="tecla para baixo<br>"}
+
+function teclacima(){
+document.getElementById("log").innerHTML+="tecla para cima<br>"
+}
